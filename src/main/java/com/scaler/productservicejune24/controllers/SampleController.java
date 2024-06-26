b8a0bda9a238afccb40a802bdff1c82a452861d3
shreya0108-tech <shreyaagarwal89.sa@gmail.com>
2024-06-26 23:56:47 +0530

    Committing

--- src/main/java/com/scaler/productservicejune24/controllers/SampleController.java
+++ src/main/java/com/scaler/productservicejune24/controllers/SampleController.java
@@ -1,2 +1,30 @@
-package com.scaler.productservicejune24.controllers;public class SampleController {
+package com.scaler.productservicejune24.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("say")
+public class SampleController {
+
+    @GetMapping("/hello/{name}/{times}")
+    public String sayHello(@PathVariable("name") String name, @PathVariable("times") int times)
+    {
+        String out = "";
+        for(int i=0;i<times;i++) {
+            out += name +" \n";
+        }
+        return out;
+    }
+
+    @GetMapping("/bye/{age}")
+    public String sayBye(@PathVariable int age)
+    {
+        return "Bye"+" "+age;
+    }
+
 }
+
+//http://localhost:4667/say/hello
